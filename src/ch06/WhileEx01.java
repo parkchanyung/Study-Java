b92cd23779c38a38047dc212243d7c5f2ba2a637
parkchanyung <cas15987@gmail.com>
2022-06-18 22:31:32 +0900

    반복문(while)

---
+++ src/ch06/WhileEx01.java
@@ -0,0 +1,15 @@
+package ch06;
+
+public class WhileEx01 {
+
+    public static void main(String[] args) {
+
+        int n = 1;
+
+        // n < 10 => 조건문
+        while(n < 10){
+            System.out.println(n);
+            n = n + 1;
+        }
+    }
+}
